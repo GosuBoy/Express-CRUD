ae1117bcc80bc5b72ee3e499e5cefdd6369d9f94
Javier_Chavez <david.o66@hotmail.com>
2023-05-16 17:56:57 -0500

    Added Base Files

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("http"); 
+
+const hostname = "127.0.0.1";
+const PORT = 3001; 
+
+const app = http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader("content-type", "text/plain"); 
+    res.end("Hello World");
+});
+
+app.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`)
+})
